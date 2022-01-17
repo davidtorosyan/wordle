9305b3660799030b04b6069096d1d8473874879f
David Torosyan <davidtorosyan.git@gmail.com>
2022-01-16 18:07:58 -0800

    Add dictionary

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A solver for [wordle](https://www.powerlanguage.co.uk/wordle/).
+
+`words_alpha.txt` sourced from [dwyl/english-words](https://github.com/dwyl/english-words).
--- src/wordle.py
+++ src/wordle.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python
 
-print("TODO")
+import os
+
+WORD_FILE_PATH = 'words_alpha.txt'
+
+def main():
+    words = load_words()
+    print('Loaded {} words.'.format(len(words)))
+
+def load_words():
+    with open(get_word_file_path()) as word_file:
+        return set(word_file.read().split())
+
+def get_word_file_path():
+    script_dir = os.path.dirname(__file__) 
+    return os.path.join(script_dir, WORD_FILE_PATH)
+
+if __name__ == '__main__':
+    main()
