fbd926d1b949ea9f921f915fcd0acdcdc8103fbc
David Torosyan <davidtorosyan.git@gmail.com>
2022-01-19 21:43:13 -0800

    Add filtering

--- src/wordle.py
+++ src/wordle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import string
 
 WORD_FILE_PATH = 'words_alpha.txt'
 WORD_LENGTH = 5
@@ -11,43 +12,58 @@ RESPONSE_RIGHT = 'g' # green
 
 class State:
     def __init__(self):
-        self.wrong = set()
-        self.close = {}
-        self.right = {}
+        self.required = {}
+        self.spots = []
+        for idx in range(0, WORD_LENGTH):
+            self.spots.append(set(string.ascii_uppercase))
 
     def fill(self, other):
-        self.wrong.update(other.wrong)
-        for (letter, indices) in other.close.items():
-            for index in indices:
-                self.mark_close(letter, index)
-        self.right.update(other.right)
+        for letter, num in other.required.items():
+            self.required[letter] = max(self.required.get(letter, 0), num)
+        for idx, spot in enumerate(self.spots):
+            spot.intersection_update(other.spots[idx])
 
-    def mark_wrong(self, letter):
-        self.wrong.add(letter)
+    def mark_wrong(self, letter, index):
+        self.spots[index].discard(letter)
 
     def mark_close(self, letter, index):
-        indices = self.close.get(letter, set())
-        indices.add(index)
-        self.close[letter] = indices
+        self.spots[index].discard(letter)
+        self.required[letter] = self.required.get(letter, 0) + 1
 
     def mark_right(self, letter, index):
-        self.right[letter] = index
+        self.spots[index].clear()
+        self.spots[index].add(letter)
+        self.required[letter] = self.required.get(letter, 0) + 1
+
+    def mark_wrong_everywhere(self, letter):
+        for spot in self.spots:
+            # don't mess with the spot if it's already solved
+            if len(spot) > 1:
+                spot.discard(letter)
 
     def is_consistent(self):
-        if self.wrong.intersection(self.close.keys()):
+        for spot in self.spots:
+            if not spot:
+                return False
+        if sum(self.required.values()) > WORD_LENGTH:
             return False
-        if self.wrong.intersection(self.right.keys()):
-            return False
-        close_indices = set((letter, index) for (letter, indices) in self.close.items() for index in indices)
-        if close_indices.intersection(self.right.items()):
-            return False
+        for letter, count in self.required.items():
+            if count > self.count_spots_with_letter(letter):
+                return False
         return True
 
+    def count_spots_with_letter(self, letter):
+        count = 0
+        for spot in self.spots:
+            if letter in spot:
+                count += 1
+        return count
+
     def __str__(self):
-        return "Right: {}, Close: {}, Wrong: {}".format(
-            self.right.__str__(),
-            self.close.__str__(),
-            self.wrong.__str__())
+        options = [''.join(sorted(spot)) for spot in self.spots]
+        return 'Required: {}, Options: {}'.format(
+            self.required.__str__(),
+            options.__str__())
 
     def __repr__(self):
         return self.__str__()
@@ -61,7 +77,10 @@ def play(words):
     knowledge = State()
     while True:
         print('Current knowledge: {}'.format(knowledge))
-        guess = get_next_word(words, knowledge).upper()
+        guess = get_next_word(words, knowledge)
+        if not guess:
+            print('No eligible guess found, we lost!')
+            break
         print('Guess: {}'.format(guess))
         response = input('Was it right? ')
         if (is_win(response)):
@@ -99,37 +118,57 @@ def parse(guess, response):
     if response is None or len(response) != WORD_LENGTH:
         return None
     result = State()
+    wrong = set()
+    close = set()
     for idx, char in enumerate(response):
         guess_char = guess[idx]
         if char == RESPONSE_WRONG:
-            result.mark_wrong(guess_char)
+            result.mark_wrong(guess_char, idx)
+            wrong.add(guess_char)
         elif char == RESPONSE_CLOSE:
             result.mark_close(guess_char, idx)
+            close.add(guess_char)
         elif char == RESPONSE_RIGHT:
             result.mark_right(guess_char, idx)
         else:
             return None
+    wrong_everywhere = wrong - close
+    for guess_char in wrong_everywhere:
+        result.mark_wrong_everywhere(guess_char)
     return result
 
-def get_next_word(words, knowledge):
-    filtered = filter_words(words, knowledge)
-    ranked = rank_words(filtered, knowledge)
+def get_next_word(words, state):
+    filtered = filter_words(words, state)
+    ranked = rank_words(filtered, state)
     return choose_word(ranked)
 
-def filter_words(words, knowledge):
-    return set([word for word in words if len(word) == WORD_LENGTH])
+def filter_words(words, state):
+    return set([word for word in words if satisfied(word, state)])
 
-def rank_words(words, knowledge):
+def satisfied(word, state):
+    if len(word) != WORD_LENGTH:
+        return False
+    for idx, char in enumerate(word):
+        if char not in state.spots[idx]:
+            return False
+    for char, count in state.required.items():
+        if word.count(char) < count:
+            return False
+    return True
+
+def rank_words(words, state):
     return {word: 1.0 for word in words}
 
 def choose_word(word_rankings):
     # sort by score, and then by word to break ties
+    if not word_rankings:
+        return None
     ranked = dict(sorted(word_rankings.items(), key=lambda item: (item[1], item[0])))
     return next(iter(ranked))
 
 def load_words():
     with open(get_word_file_path()) as word_file:
-        return set(word_file.read().split())
+        return set(word_file.read().upper().split())
 
 def get_word_file_path():
     script_dir = os.path.dirname(__file__) 
